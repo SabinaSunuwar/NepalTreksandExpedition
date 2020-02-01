cd11195623ff7e5ffaf8b1d8171d5c32290aede2
sabinasunuwar <sabinasunuwar4@gmail.com>
2020-02-01 10:57:31 +0545

    Trek schema added

---
+++ models/treks.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const trekSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        minlength: 6
+    },
+    days: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: String,
+        required: true
+    },
+    itinerary: {
+        type: String,
+        required: true
+    }
+});
+
+module.exports = mongoose.model('Trek', trekSchema);
